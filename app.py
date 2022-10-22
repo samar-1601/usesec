e7939baa151f8bf3eaa3a93a8e944e7cb221f969
samar-1601 <samar.pp.singh@gmail.com>
2022-10-23 03:47:15 +0530

    flask files added

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+
+@app.route("/", methods=["GET"])
+@cross_origin(supports_credentials=True)
+def sendQuestions():
+    print("called")
+    questions = {
+        "text": [
+            {"id": 1, "question": "How r you"},
+            {"id": 2, "question": "How r you"},
+            {"id": 3, "question": "How r you"},
+            {"id": 4, "question": "How r you"},
+            {"id": 5, "question": "How r you"},
+            {"id": 6, "question": "How r you"},
+            {"id": 7, "question": "How r you"},
+        ],
+        "facebook": [
+            {"id": 1, "question": "How r you"},
+            {"id": 2, "question": "How r you"},
+            {"id": 3, "question": "How r you"},
+        ],
+        "liker": [
+            {"id": 1, "question": "How r you"},
+            {"id": 2, "question": "How r you"},
+            {"id": 3, "question": "How r you"},
+        ],
+    }
+    return jsonify(questions)
+
+
+@app.route("/<type>/<id>/<ans>", methods=["POST", "GET"])
+def incrementer(id="", ans=""):
+    return "Incremented number is " + str(id) + ans
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
