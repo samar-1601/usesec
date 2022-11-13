3bbb4eb96cdc4838a89cf002b270c2d5ab080af7
preyam2002 <preyam2002@gmail.com>
2022-11-14 01:12:40 +0530

    added comment api

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import jsonify
 from flask_cors import CORS, cross_origin
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -35,6 +36,22 @@ def sendQuestions():
     import json
     database = json.load(open("static/data.json"))
     return database
+
+@app.route("/get_comments", methods=["GET"])
+def get_comments():
+    import json
+    database = json.load(open("static/data.json"))
+    comments = {}
+    comments["data"] = []
+    for comment_category in database["data"]:
+        sz = len(comment_category["data"])
+        number = 1
+        if comment_category["category"] == "Random":
+            number = 3
+        rand_numbers = random.sample(range(0, sz-1), number)
+        for rand_number in rand_numbers:
+            comments["data"].append(comment_category["data"][rand_number])
+    return comments
 
 @app.route("/<file_image_path>", methods=["GET"])
 def get_image(file_image_path):
