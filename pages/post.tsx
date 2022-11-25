45909c7b7cf5e836eefa07383d919779cdb9176e
Aryan Agarwal <aryan.cs.kgp@gmail.com>
2022-11-25 20:03:15 +0530

    fix useeffect

--- pages/post.tsx
+++ pages/post.tsx
@@ -5,9 +5,7 @@ import Post from "../components/Post/Post";
 
 const PostPage = () => {
   const [isPost, setLoading] = useState<boolean>(true);
-  const [questions, setQuestions] = useState([]);
   const [questionIndex,setQuestionIndex] = useState(0);
-
 
   const [questionText,setQuestionText] = useState("");
   const [comment_1,setComment_1] = useState("");
@@ -15,18 +13,21 @@ const PostPage = () => {
   const [comment_3,setComment_3] = useState("");
   const [img_url,setImg_url] = useState("");
 
+  let questions = [];
+
+  if (typeof window !== 'undefined') {
+    questions = JSON.parse(localStorage.getItem("questions")).data
+  }
+
   
   useEffect(() => {
-    setQuestions(JSON.parse(localStorage.getItem("questions")).data);
-
-    if(questions.length>0){
+    if(questionIndex < questions.length){
       setQuestionText(questions[questionIndex].main_text);
       setComment_1(questions[questionIndex].comment_1_girl);
       setComment_2(questions[questionIndex].comment_2);
       setComment_3(questions[questionIndex].comment_3);
       setImg_url("https://usesec-backend.herokuapp.com/" + questions[questionIndex].image);
     }    
-    
   }, []);
 
   return !isPost ? (
