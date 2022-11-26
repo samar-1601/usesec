8c02d8c9509bcc40234ff79d8d37ea487084a763
preyam2002 <preyam2002@gmail.com>
2022-11-26 19:48:19 +0530

    made little better

--- app.py
+++ app.py
@@ -43,11 +43,6 @@ def get_comments():
 
 @app.route("/post_answer", methods=["GET", "POST"])
 def post_answer():
-    # response = {}
-    # response["question_type"] = request.args.get('question_type')
-    # response["question_id"] = request.args.get('question_id')
-    # response["question"] = request.args.get('question')
-    # response["answer"] = request.args.get('answer')
     response  = request.data
     request_url = 'https://api.mailgun.net/v2/{0}/messages'.format(SANDBOX_URL)
     email_response = requests.post(request_url, auth=('api', API_KEY), data={
